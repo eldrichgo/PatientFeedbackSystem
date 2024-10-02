a2a4bb83eb6e69a548d405a14c84ab78ba14c6bc
EJ <eldrich1212@gmail.com>
2024-10-02 17:23:54 +0800

    containerization of feedback and survey services using docker

--- server/services/feedback/server.go
+++ server/services/feedback/server.go
@@ -32,8 +32,8 @@ func initLogger() logger.Interface {
 }
 
 func main() {
-	dsn := "host=localhost user=postgres password=1234 dbname=FeedbackDB port=5432 sslmode=prefer TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: initLogger()})
+	// dsn := "host=localhost user=postgres password=1234 dbname=FeedbackDB port=5432 sslmode=prefer TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open("postgresql://postgres:1234@db:5432/feedbackDB"), &gorm.Config{Logger: initLogger()})
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
